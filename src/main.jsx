874e0b38a9f7fe67df1d87cd184667d865086ce6
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-10 17:58:41 +0600

    Home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router/Route.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <div className="max-w-screen-xl mx-auto	">
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </div>
+);

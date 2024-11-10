cd287e07fc81dd9c7aadf7757aa37209d7191023
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-10 17:58:13 +0600

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
 import React from 'react';
+import Header from './shared/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from './shared/Footer';
 
 const App = () => {
   return (
-    <div className='text-3xl'>
-      tanvir
+    <div className=''>
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
   );
 };

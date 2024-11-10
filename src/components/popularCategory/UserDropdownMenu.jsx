0ff824a373be4c6c5b2578deb3f2a3e17868fe43
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-10 19:39:21 +0600

    user dropdown

---
+++ src/components/popularCategory/UserDropdownMenu.jsx
@@ -0,0 +1,37 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+const UserDropdownMenu = () => {
+  const [position, setPosition] = useState("bottom");
+  return (
+    <div className="">
+        <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <img
+          className="w-10 rounded"
+          src="https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg"
+          alt=""
+        />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56 ml-1 md:mr-20">
+        <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+          <DropdownMenuRadioItem value="top">Top</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="bottom">Bottom</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="right">Right</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+    </div>
+  );
+};
+
+export default UserDropdownMenu;

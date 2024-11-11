723bf459363658e3b015fd4c43848b837be98619
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-11 20:02:47 +0600

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 
 import Header from './shared/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Footer from './shared/Footer';
 
 const App = () => {
+  const location = useLocation();
+  const path = location.pathname.includes('register') || location.pathname.includes('login') 
   return (
     <div className=''>
-      <Header />
+     { path || <Header />}
       <Outlet />
-      <Footer />
+      {path || <Footer />}
    </div>
   );
 };

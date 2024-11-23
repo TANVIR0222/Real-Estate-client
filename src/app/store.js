1f0c738b79be018fa0f231e17ac0b597882efdc9
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-23 17:58:37 +0600

    add auth Slice

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 
 import { authApi } from './feature/auth/authApi'
+import  authReducer  from './feature/auth/authSlice'
 
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
   },
 

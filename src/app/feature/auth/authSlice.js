0068a005dd887f05b12ef7f05d01deea82bbac74
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-23 17:59:09 +0600

    add  auth Slice

---
+++ src/app/feature/auth/authSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const loadUserFormLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem("user");
+    if (serializedState === null) return { user: null };
+
+    return { user: JSON.parse(serializedState) };
+  } catch (error) {
+    return { user: null };
+  }
+};
+
+const initialState = loadUserFormLocalStorage();
+
+export const authSlice = createSlice({
+  name: "authSlice",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload.user;
+      console.log(state.user);
+      
+      localStorage.setItem("user", JSON.stringify(state.user));
+    },
+    logout: (state) => {
+      state.user = null;
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { setUser, logout } = authSlice.actions;
+
+export default authSlice.reducer;

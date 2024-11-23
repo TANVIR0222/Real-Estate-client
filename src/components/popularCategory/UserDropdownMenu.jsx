6777ccbe5cecadb1f4ada796a9cdc7eacfd55a43
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-23 18:14:06 +0600

    User Dropdown Menu  Page

--- src/components/popularCategory/UserDropdownMenu.jsx
+++ src/components/popularCategory/UserDropdownMenu.jsx
@@ -1,3 +1,5 @@
+import { useUserLogoutMutation } from "@/app/feature/auth/authApi";
+import { logout } from "@/app/feature/auth/authSlice";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,28 +10,47 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 const UserDropdownMenu = () => {
   const [position, setPosition] = useState("bottom");
+
+  const [userLogout]  = useUserLogoutMutation();
+  const dispatch = useDispatch();
+
+  const handleLogout = async() => {
+    try {
+     await userLogout().unwrap();
+      dispatch(logout());
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
   return (
     <div className="">
-        <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <img
-          className="w-10 rounded"
-          src="https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg"
-          alt=""
-        />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56 ml-1 md:mr-20">
-        <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-          <DropdownMenuRadioItem value="top">Top</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="bottom">Bottom</DropdownMenuRadioItem>
-          <DropdownMenuRadioItem value="right">Right</DropdownMenuRadioItem>
-        </DropdownMenuRadioGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <img
+            className="w-10 rounded"
+            src="https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg"
+            alt=""
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56 ml-1 md:mr-20">
+          <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+            <DropdownMenuRadioItem value="top">Add a Property</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="bottom">Trip List</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="right">Wish Lish</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="right">Property List</DropdownMenuRadioItem>  
+            <DropdownMenuRadioItem value="right">Reservation List</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem onClick ={handleLogout} value="right">Logout</DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };

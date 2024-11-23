5f08b15ede60a09e0fe92f67d0e1660958294007
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-23 18:05:48 +0600

    add redux store

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router/Route.jsx";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 createRoot(document.getElementById("root")).render(
   <div className="max-w-screen-xl mx-auto bg-primarybg	">
     <StrictMode>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </StrictMode>
   </div>
 );

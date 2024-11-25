521958a751aa5f3688b42ba01f472d4add00ac1b
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-25 23:50:10 +0600

    add Property Api

--- src/app/store.js
+++ src/app/store.js
@@ -3,16 +3,18 @@ import { setupListeners } from '@reduxjs/toolkit/query'
 
 import { authApi } from './feature/auth/authApi'
 import  authReducer  from './feature/auth/authSlice'
+import { propertyApi } from './feature/property/propertyApi'
 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
+    [propertyApi.reducerPath]: propertyApi.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware,propertyApi.middleware,),
 })
 
 

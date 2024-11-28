d64e9494a4deb39e2c54e94e615d73c9bfae3980
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-28 23:48:46 +0600

    property Api

---
+++ src/app/feature/propertyApi/propertyApi.js
@@ -0,0 +1,26 @@
+import getBaseUrl from '@/utils/getBaseUrl'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const propertyApi = createApi({
+  reducerPath: 'propertyApi',
+  baseQuery: fetchBaseQuery({ baseUrl: `${getBaseUrl()}/api/post/` }),
+  endpoints: (builder) => ({
+    addProperty: builder.mutation({
+      query: (newData) => ({
+        url: `add-property`,
+        method: 'POST',
+        body: newData,
+      }),
+    }),
+    categoryFilter: builder.query({
+      query: (category) => `category?category=${category}`
+    }),
+    singleCategoryProperty: builder.query({
+      query: (_id) => ({
+        url: `single-Property/${_id}`,
+      }),
+    }),
+  }),
+})
+
+export const { useAddPropertyMutation , useCategoryFilterQuery , useSingleCategoryPropertyQuery } = propertyApi

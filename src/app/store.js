49a41ce99495c998eb10828ba7f77b5f0e491bc5
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-28 23:47:34 +0600

    store

--- src/app/store.js
+++ src/app/store.js
@@ -3,18 +3,24 @@ import { setupListeners } from '@reduxjs/toolkit/query'
 
 import { authApi } from './feature/auth/authApi'
 import  authReducer  from './feature/auth/authSlice'
-import { propertyApi } from './feature/property/propertyApi'
+import { propertyApi } from './feature/propertyApi/propertyApi'
+import { imageApi } from './feature/imageUploade/imageApi'
+import  propertyReducer  from './feature/propertyApi/propertySlice'
+import { bookingApi } from './feature/bookingApi/bookingApi'
 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    property: propertyReducer,
     [authApi.reducerPath]: authApi.reducer,
     [propertyApi.reducerPath]: propertyApi.reducer,
+    [imageApi.reducerPath]: imageApi.reducer,
+    [bookingApi.reducerPath]: bookingApi.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware,propertyApi.middleware,),
+    getDefaultMiddleware().concat(authApi.middleware,propertyApi.middleware,imageApi.middleware,bookingApi.middleware),
 })
 
 

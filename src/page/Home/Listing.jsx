19f044e40442bef794b8195536a221eed808ccce
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-28 23:52:53 +0600

    Listing page

--- src/page/Home/Listing.jsx
+++ src/page/Home/Listing.jsx
@@ -2,10 +2,13 @@ import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
 import { GiBoatFishing, GiCactus, GiIsland, GiWindmill } from "react-icons/gi";
 import { FaSkiing } from "react-icons/fa";
 import { BiWorld } from "react-icons/bi";
-
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
-
 import { MdOutlineVilla } from "react-icons/md";
+import { useState } from "react";
+import { useCategoryFilterQuery } from "@/app/feature/propertyApi/propertyApi";
+import { Link } from "react-router-dom";
+import ListCart from "@/components/popularCategory/ListCart";
+import Loading from "@/components/popularCategory/Loading";
 
 const categories = [
   {
@@ -61,6 +64,10 @@ const categories = [
 ];
 
 const Listing = () => {
+  const [category, setSelectedCategory] = useState("All");
+  const { data: property, isLoading } = useCategoryFilterQuery(category);
+  // console.log(property);
+
   return (
     <div className="pb-16 text-center items-center">
       <div className="text-center pb-16">
@@ -74,24 +81,42 @@ const Listing = () => {
           <div className="flex gap-4  p-5 ">
             {categories.map((artwork) => (
               <figure key={artwork.label} className={`shrink-0 mx-auto`}>
-                <div className="overflow-hidden  rounded-full h-15 w-15">
-                  <div
-                    className="items-center text-center flex p-2  justify-center"
-                    style={{ backgroundColor: `${artwork.color}` }}
-                  >
-                    {artwork.icon}
+                <Link>
+                  <div className="overflow-hidden  rounded-full h-15 w-15">
+                    <div
+                      className="items-center text-center flex p-2  justify-center"
+                      style={{ backgroundColor: `${artwork.color}` }}
+                    >
+                      {artwork.icon}
+                    </div>
                   </div>
-                </div>
-                <figcaption className="pt-2 text-xs text-muted-foreground">
-                  <span className="font-semibold text-foreground">
-                    {artwork.label}
-                  </span>
-                </figcaption>
+                  <figcaption className="pt-2 text-xs text-muted-foreground">
+                    <span
+                      onClick={() => setSelectedCategory(artwork.label)}
+                      className="font-semibold text-foreground"
+                    >
+                      {artwork.label}
+                    </span>
+                  </figcaption>
+                </Link>
               </figure>
             ))}
           </div>
           <ScrollBar orientation="horizontal" />
         </ScrollArea>
+      </div>
+      {/* listings */}
+
+      <div className="grid  grid-cols-1 md:grid-cols-2">
+        {isLoading ? (
+          <Loading />
+        ) : (
+          property?.map((item) => (
+            <div key={item._id} className="flex flex-col ">
+              <ListCart property={item} />
+            </div>
+          ))
+        )}
       </div>
     </div>
   );

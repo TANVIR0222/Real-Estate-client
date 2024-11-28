a607bb1db9283c23d08d8ab5d76f343c622ec21f
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-28 23:50:11 +0600

    Loading

---
+++ src/components/popularCategory/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-12 h-12 border-b-2 border-gray-900 border-solid rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loading;

4d7ddcbfbdb02d02452b047c38c14a7519b4d059
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-29 21:13:53 +0600

    Loading Page

--- src/components/popularCategory/Loading.jsx
+++ src/components/popularCategory/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center h-screen">
       <div className="w-12 h-12 border-b-2 border-gray-900 border-solid rounded-full animate-spin"></div>
     </div>
   );

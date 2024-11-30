e070431f203b025e1a771f07728d0f22b0f8dbe4
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-11-30 15:08:32 +0600

    search

--- src/components/popularCategory/Search.jsx
+++ src/components/popularCategory/Search.jsx
@@ -1,12 +1,20 @@
 import { Input } from "../ui/input";
 import { CiSearch } from "react-icons/ci";
+import { Link } from "react-router-dom";
 
 const Search = () => {
+  
   return (
-    <div className="flex items-center">
-      <Input  type="text" placeholder="Search..." />
-      <CiSearch className="-ml-5" />
-    </div>
+    <Link to={'/search-view'}>
+      <div className="flex items-center">
+        <Input
+          type="text"
+          placeholder="Search..."
+          className="bg-slate-50 text-green"
+        />
+        <CiSearch className="-ml-5 text-black" />
+      </div>
+    </Link>
   );
 };
 
